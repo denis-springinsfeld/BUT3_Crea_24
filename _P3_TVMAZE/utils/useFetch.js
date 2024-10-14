c198b7499cb52db5e4e6c4c671ae700c98534bd0
Springinsfeld Denis <denis.springinsfeld@unilim.fr>
2024-10-14 18:46:35 +0200

    P3

---
+++ _P3_TVMAZE/utils/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Problème avec la requête");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
